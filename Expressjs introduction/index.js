f1f0facec459a9a1eda204fd1e9fa45c9a75fbca
Megha <meghapatel1.tops@gmail.com>
2025-01-09 14:40:14 +0530

    mondodb connectivity & crud

--- Expressjs introduction/index.js
+++ Expressjs introduction/index.js
@@ -3,6 +3,9 @@ var dotenv = require('dotenv').config();
 const userRouter = require('./Route/Userroute')
 const hbs = require('hbs')
 const path = require('path')
+var axios = require('axios')
+const getWhetherApp = require('./src/getwhether')
+const Db = require('./db');
 
 const app = express();
 
@@ -17,9 +20,11 @@ const errorMiddleware = (err,req,res,next)=>{
     res.status(500).send("Smoting Wrong")
 }
 
+app.use('/public',express.static(path.join(__dirname,'public')))
 app.set('views', path.join(__dirname,"views"))
 console.log(path.join(__dirname,'views'))
 app.set('view engine','hbs')
+hbs.registerPartials(path.join(__dirname,'./views/partials'))
 
 //app.use(errorMiddleware);
 //app.use(logMiddleware);
@@ -42,6 +47,55 @@ app.get('/products',(req,res)=>{
     ];
     res.render('product',{productsdata:products})
 })
+
+app.get('/getWhetherApp/:city',(req,res)=>{
+    city = req.params.city;
+    getWhetherApp.getDataByCity(city,(data,err)=>{
+        if(data){
+           getWhetherApp.getwhetherCall(data.lat,data.lon,(data1,err)=>{
+                if(data1){
+                    res.send(data1);
+                }
+                else{
+                    res.status(500).send({"msg":"Something wrong"})
+                }
+           })
+        }
+        else{
+            res.status(500).send({"msg":"Something wrong"})
+        }
+    })
+    
+})
+
+app.get('/aboutpage',(req,res)=>{
+    res.render('About');
+})
+
+app.get('/citydata/:city',(req,res)=>{
+    const city =req.params.city;
+    getWhetherApp.getDataByCity(city,(data,err)=>{
+        if(data){
+            getWhetherApp.getwhetherCall(data.lat,data.lat,(data1,err)=>{
+                if(data1){
+                    res.status(200).send(data1);
+                }
+                else{
+                    res.status(500).send({"msg":"Something wrong"})
+                }
+            })
+        }
+        else{
+            res.status(500).send({"msg":"Something wrong"})
+        }
+    })
+})
+
+app.get('/connectdb',(req,res)=>{
+    Db.updateData()
+})
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log("project working on 3000");
